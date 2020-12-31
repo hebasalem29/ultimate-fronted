f65ea81816266a2fa5f09809a947a64c0d080406
hebasalem29 <hebanassr58@gmail.com>
2020-12-31 08:45:35 +0200

    five

--- stage/js/main.js
+++ stage/js/main.js
@@ -9,5 +9,44 @@ $(function()
     {
         $(this).toggleClass("fa-angle-right fa-angle-down");
         $(this).next(".child-links").toggleClass("hide");
-    })
-})
+    });
+    $(".toggle-fullscreen").on("click",function()
+    {
+$(this).toggleClass("full-screen");
+if($(this).hasClass('full-screen'))
+{
+    openFullscreen();
+
+}
+else
+{
+    closeFullscreen()
+}
+
+
+
+
+    });
+});
+var elem = document.documentElement;
+
+/* When the openFullscreen() function is executed, open the video in fullscreen.
+Note that we must include prefixes for different browsers, as they don't support the requestFullscreen method yet */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.requestFullscreen) { /* IE11 */
+    elem.requestFullscreen();
+  }
+}
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.exitFullscreen) { /* IE11 */
+      document.exitFullscreen();
+    }
+  }
